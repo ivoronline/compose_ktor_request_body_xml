b8b7930be2db0d45c8df5710607c949f1fe09c2a
igoreta <ivoronline@gmail.com>
2022-05-24 09:24:12 +0200

    Initial commit

---
+++ app/src/main/java/com/ivoronline/compose_ktor_request_body_xml/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.ivoronline.compose_ktor_request_body_xml
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.serialization.kotlinx.xml.*
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+
+//==================================================================
+// MAIN ACTIVITY
+//==================================================================
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+
+      var responseBody by remember { mutableStateOf("") }
+      val coroutineScope = rememberCoroutineScope()
+
+      Button(onClick = { coroutineScope.launch { responseBody = callURL() } }) {
+        Text("RESPONSE: $responseBody")
+      }
+
+    }
+  }
+}
+
+//==================================================================
+// CALL URL
+//==================================================================
+suspend fun callURL() : String  {
+
+  //CONFIGURE CLIENT
+  val client = HttpClient(CIO) {
+    install(ContentNegotiation) { xml() }
+  }
+
+  //CAL URL
+  val httpResponse: HttpResponse = client.post("http://192.168.0.102:8080/SendBodyXML") {
+    contentType(ContentType.Application.Xml)
+    setBody(PersonDTOXML(1, "John", 20))
+  }
+
+  //CLOSE CLIENT
+  client.close()
+
+  //RETURN PERSON
+  return httpResponse.body();
+
+}
+
+//==================================================================
+// PERSON
+//==================================================================
+@Serializable
+data class PersonDTOXML(val id: Int, val name: String, val age: Int)
